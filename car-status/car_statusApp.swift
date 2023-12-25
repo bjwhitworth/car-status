7c20c3cd3f8f4d008d90effe436a6fcefee1df4a
Ben W <benwhitworth86@btinternet.com>
2023-12-25 20:37:42 +0000

    Initial Commit

---
+++ car-status/car_statusApp.swift
@@ -0,0 +1,17 @@
+//
+//  car_statusApp.swift
+//  car-status
+//
+//  Created by Benjamin Whitworth on 25/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct car_statusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
